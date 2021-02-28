e9665b697a471c75e91dc6452b6bacacc51f6ad8
maribels_are_ringing <maribel.duron86@gmail.com>
2021-02-28 16:54:23 -0700

    added css and timer function with buttons

---
+++ assets/js/quiz.js
@@ -0,0 +1,101 @@
+var body = document.body;
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+var button = document.getElementById('button');
+var questionEl = document.getElementById('question')
+var answerBtnsEl = document.getElementById('answerBtns')
+// var message = 'Your score is';
+
+var h3ScoreEl = document.createElement('h3Score');
+h3ScoreEl.textContent = 'View High Scores';
+h3ScoreEl.setAttribute('style', 'margin:auto; width:75%; text-align:left;');
+body.appendChild(h3ScoreEl);
+
+// Start quiz button and start and countdown functions
+button.addEventListener('click', startQuiz)
+button.addEventListener('click', countdown)
+
+function startQuiz() {
+    button.classList.add('hide')
+    showQuestions()
+}
+
+function showQuestions(questions) {
+    questionEl.innerText = questions.questions
+}
+
+// Timer that counts down from 60 seconds
+function countdown() {
+    var timeLeft = 5;
+
+  // executed every 1000 milliseconds
+  var timeInterval = setInterval(function() {
+    // If statement for timer with greater than 1 sec remaining
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      timerEl.textContent = 'Time is up!';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+      displayMessage();
+    }
+  }, 1000);
+}
+
+// Select answer function
+function answer() {
+
+}
+
+// The array of questions for the game.
+var questions = [
+    { q: 'An array holds _____.', 
+    a: [
+        {text: 'functions', correct: false},
+        {text: 'variables', correct: false}, 
+        {text:'values', correct: true},
+        {text: 'cars', correct: false},
+    ]
+    },
+
+    { q: 'Which conditional statement executes many alternative blocks of code?', 
+    a: [
+        {text: 'if', correct: false},
+        {text: 'else', correct: false}, 
+        {text:'else if', correct: false},
+        {text: 'switch', correct: true},
+    ]
+    },
+
+    { q: 'The Math.floor() object rounds a number ______.', 
+    a: [
+        {text: 'up', correct: false},
+        {text: 'down', correct: true}, 
+        {text:'sideways', correct: false},
+        {text: 'none of the above', correct: false},
+    ]
+},
+
+    { q: 'A pseudo-class defines a _____ ____ of an element.', 
+    a: [
+        {text: 'time sensitivity', correct: false},
+        {text: 'cloud app', correct: false}, 
+        {text:'web api', correct: false},
+        {text: 'special state', correct: true},
+    ]
+    },
+
+    { q: 'JavaScript is the easiest programming language you will learn.', 
+    a: [
+        {text: 'yes', correct: true},
+        {text: 'no', correct: true}, 
+        {text:'maybe', correct: true},
+        {text: 'ask me later', correct: true},
+    ]
+    },
+]
